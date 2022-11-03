358790552052daad3d98d3858af52a01af15542f
Sergey <s_fedorow@mail.ru>
2022-11-03 21:22:44 +0600

    completed catalog and almost completed product page

--- src/js/files/forms/range.js
+++ src/js/files/forms/range.js
@@ -10,8 +10,8 @@ import * as noUiSlider from 'nouislider';
 export function rangeInit() {
   const priceSlider = document.querySelector('[data-range]');
   if (priceSlider) {
-    const textFrom = priceSlider.querySelector('[data-range-from]');
-    const textTo = priceSlider.querySelector('[data-range-to]');
+    const textFrom = priceSlider.querySelector('[data-range-from]');    
+    const textTo = priceSlider.querySelector('[data-range-to]');    
     let item = priceSlider.querySelector('[data-range-item]');
     noUiSlider.create(item, {
       start: [Number(textFrom.value), Number(textTo.value)],
@@ -22,9 +22,78 @@ export function rangeInit() {
 				'max': [Number(textTo.dataset.rangeTo)]
 			}
     });
+    const snapValues = [
+      textFrom,
+      textTo
+    ];
     item.noUiSlider.on('update', function (values, handle) {
-      textFrom.value = values[handle];
-      textTo.value = values[handle];
+      snapValues[handle].value = values[handle];
+  });
+  }
+
+  const lengthSlider = document.querySelector('[data-length]');
+  if (lengthSlider) {
+    const textFrom = lengthSlider.querySelector('[data-length-from]');    
+    const textTo = lengthSlider.querySelector('[data-length-to]');    
+    let item = lengthSlider.querySelector('[data-length-item]');
+    noUiSlider.create(item, {
+      start: [Number(textFrom.value), Number(textTo.value)],
+			connect: true,      
+			range: {
+				'min': [Number(textFrom.dataset.lengthFrom)],
+				'max': [Number(textTo.dataset.lengthTo)]
+			}
+    });
+    const snapValues = [
+      textFrom,
+      textTo
+    ];
+    item.noUiSlider.on('update', function (values, handle) {
+      snapValues[handle].value = values[handle];
+  });
+  }
+
+  const lengthBladeSlider = document.querySelector('[data-blide]');
+  if (lengthBladeSlider) {
+    const textFrom = lengthBladeSlider.querySelector('[data-blide-from]');    
+    const textTo = lengthBladeSlider.querySelector('[data-blide-to]');    
+    let item = lengthBladeSlider.querySelector('[data-blide-item]');
+    noUiSlider.create(item, {
+      start: [Number(textFrom.value), Number(textTo.value)],
+			connect: true,      
+			range: {
+				'min': [Number(textFrom.dataset.blideFrom)],
+				'max': [Number(textTo.dataset.blideTo)]
+			}
+    });
+    const snapValues = [
+      textFrom,
+      textTo
+    ];
+    item.noUiSlider.on('update', function (values, handle) {
+      snapValues[handle].value = values[handle];
+  });
+  }
+
+  const widthBladeSlider = document.querySelector('[data-width]');
+  if (widthBladeSlider) {
+    const textFrom = widthBladeSlider.querySelector('[data-width-from]');    
+    const textTo = widthBladeSlider.querySelector('[data-width-to]');    
+    let item = widthBladeSlider.querySelector('[data-width-item]');
+    noUiSlider.create(item, {
+      start: [Number(textFrom.value), Number(textTo.value)],
+			connect: true,      
+			range: {
+				'min': [Number(textFrom.dataset.widthFrom)],
+				'max': [Number(textTo.dataset.widthTo)]
+			}
+    });
+    const snapValues = [
+      textFrom,
+      textTo
+    ];
+    item.noUiSlider.on('update', function (values, handle) {
+      snapValues[handle].value = values[handle];
   });
   }
 }
